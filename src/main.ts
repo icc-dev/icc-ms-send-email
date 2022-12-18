2d3735749d03f32965a0e914999e199d9d2bc66e
IanCardenasCastillo <iancardenas96@gmail.com>
2022-12-18 19:59:24 -0300

    init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './main/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    {
+      transport: Transport.TCP
+    }
+  );
+  await app.listen();
+}
+bootstrap();
